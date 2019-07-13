4fb483fc6b4ad075439f79e978b83f17819aece2
raim <raim@tbi.univie.ac.at>
2019-07-13 20:05:11 +0200

    adding code

---
+++ R/genex.R
@@ -0,0 +1,56 @@
+library(gsl) # hypergeometric function solver
+Gauss2F1 <- function(a,b,c,x, method=c("forrey","laurent") ){
+  
+  if ( sum(c(length(a)>1, length(b)>1, length(c)>1, length(x)>1))>1 )
+    stop("Gauss2F1: only one parameter can be a vector")
+  ## NOTE: for |z|>1 we use the transformation recommended in
+  ## R.C. Forrey: Computing the Hypergeometric Function
+  ## JOURNAL OF COMPUTATIONAL PHYSICS137,79–100 (1997)
+  ## Stephane Laurent's version from stackoverflow seems both more
+  ## efficient and also much more stable for some small delta
+  y <- rep(list(NA),length(x))
+  for ( i in 1:length(x) )
+    if( abs(x[i]) < 1 ) {
+      y[[i]] <- hyperg_2F1(a,b,c,x[i])
+    } else {
+        if ( method[1]=="forrey" ) {
+            w <- 1/(1-x[i])
+            A <- w^a*gamma(c)*gamma(b-a)/(gamma(b)*gamma(c-a))*hyperg_2F1(a,c-b,a-b+1,w)
+            B <- w^b*gamma(c)*gamma(a-b)/(gamma(a)*gamma(c-b))*hyperg_2F1(b,c-a,b-a+1,w)
+            ##if ( is.na(A) ) A <- 0
+            ##if ( is.na(B) ) B <- 0
+            y[[i]] <- A+B
+        } else if (method[1]=="laurent" ) # Stephane Laurent's version
+            y[[i]] <- hyperg_2F1(c-a,b,c,1-1/(1-x[i]))/(1-x[i])^b 
+    }
+  unlist(y)
+}
+## induced protein expression in growing cultures, with half-life of inducer
+## analytic solution, protein
+fexpr <- function(delta, time=seq(1,10,.1), I0=10, y0=50,
+                  n=2, K=100, l=1, v=100, beta=.1,
+                  method=c("forrey","laurent")) {
+  
+  y <- rep(list(NA),length(I0))
+  for ( i in 1:length(I0) ) {
+    i0 <- I0[i]  
+    if ( delta == 0 | i0 == 0) {
+      A <- i0^n/(i0^n + K^n) # induced transcription 
+    } else {
+      A <- Gauss2F1(1, 
+                    beta/(n*delta),
+                    beta/(n*delta)+1, 
+                    -exp(n*delta*time)*K^n/i0^n, method=method[1])
+    }
+    y[[i]] <- y0*exp(-beta*time) + (l + v*A)/beta * (1-exp(-beta*time)) 
+  }
+  y <- unlist(y)
+  ## REMOVE y<0 - not physical, numerical artefact
+  if ( any(!is.na(y)) )
+    if ( any(y<0, na.rm=TRUE) ) {
+      warning(sum(y<0,na.rm=TRUE), " y<0 replaced by NA")
+      y[which(y<0)] <- NA
+    }
+  y
+}
+
---
+++ doc/analyse.R
@@ -0,0 +1,77 @@
+source("../R/genex.R")
+
+### GENERAL EXPERIMENT PARAMETERS 
+
+## growth and protein degradation rates
+mu <- 0.029 # h-1, 1 division per day - mu = log(2)/24
+dy <- 0.07  # h-1, protein degradation rate, ASV-tagged mVenus
+beta <- mu + dy
+
+### INDUCER-SPECIFIC PARAMETERS
+y0 <- 50 # intial protein concentration
+n <- 2   # Hill factor, ~ number of binding sites 
+K <- 100 # half-maximal concentration 
+l <- 1   # uninduced "leaky" transcription
+v <- 100 # maximal induced transcription
+
+## INDUCER HALF-LIFE - constant inducer over time
+tI <- 17.5 # h, half-life aTc
+delta <- log(2)/tI # h-1, degradation rate aTc
+#delta <- 0 # for rhamnose?
+
+## NOTE: with current parameters, we already run into convergence problems at delta~0.001
+#delta <- 0.011
+
+
+
+par(mfcol=c(2,1), mai=c(.5,.5,.1,.1), mgp=c(1.3,.4,0),tcl=-.25)
+i0 <- c(10000, 100)
+ts <- c(24,48)
+yt1 <- fexpr(time=time, I0=i0[1], delta=delta, beta=beta, 
+             y0=y0, n=n, K=K, l=l, v=v)
+yt2 <- fexpr(time=time, I0=i0[2], delta=delta, beta=beta, 
+             y0=y0, n=n, K=K, l=l, v=v)
+yi1 <- fexpr(time=ts[2], I0=I0, delta=delta, beta=beta, 
+             y0=y0, n=n, K=K, l=l, v=v)
+yi2 <- fexpr(time=ts[1], I0=I0, delta=delta, beta=beta, 
+             y0=y0, n=n, K=K, l=l, v=v)
+
+ys <- c(yt1,yt2,yi1,yi2)
+ys <- ys[is.finite(ys)]
+ylim <- c(0, max(ys,na.rm=TRUE))
+
+plot(time, yt1, type="b", pch=19, cex=.5, ylim=ylim,
+     ylab="fluorescence/OD, a.u.", xlab="time, h")
+lines(time, yt2, col=2, type="b", pch=19, cex=.5)
+legend("topright",legend=c(as.expression(bquote(I[0] ~":"~.(i0[1]))),
+                           bquote(I[0]~":"~.(i0[2]))),
+       title=paste0("half-life: ",tI, ",h"), col=1:2,lty=1,pch=19,pt.cex=.5)
+abline(v=ts, col=3:4, lty=1)
+plot(I0, yi1, type="b", col=4, lty=1, pch=19, cex=.5,
+     log="x",ylim=ylim,xlim=c(1,max(I0)),
+     xlab="inducer concentration", ylab="fluorescence/OD, au")
+lines(I0, yi2, type="b", col=3, lty=1, pch=19, cex=.5)
+abline(v=i0,col=1:2)
+legend("bottomright",paste("at",ts,"h"),
+       title=paste0("half-life: ",tI, ",h"),
+       col=3:4,lty=1,pch=19,pt.cex=.5)
+
+time <- 0:100
+plot(1, col=NA,  ylim=ylim,xlim=range(time),
+     xlab="time, h", ylab="fluorescence/OD, au")
+for ( delta in seq(0,5,.05)) {
+    ## NOTE: instabilities also at higher delta!
+    yt1 <- fexpr(time=time, I0=1000, delta=delta, beta=beta, 
+                 y0=y0, n=n, K=K, l=l, v=v, method="laurent")
+    lines(time,yt1)
+}
+
+I0 <- seq(0,1000,10) #c(0,10^seq(-2,4,.1))
+plot(1, col=NA, ylim=ylim, xlim=c(0.1,max(I0)),
+     xlab="inducer concentration", ylab="fluorescence/OD, au")     
+for ( tm in seq(0,72,5)) {
+  yt1 <- fexpr(time=tm, I0=I0, delta=log(2)/24, beta=beta, 
+               y0=y0, n=n, K=K, l=l, v=v, method="laurent")
+  lines(I0,yt1)
+}
+
